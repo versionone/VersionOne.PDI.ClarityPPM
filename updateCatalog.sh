756b8e6c7e81c195c3e0114b29625902a1d073e4
Josh Gough <jsgough@gmail.com>
2013-04-16 15:35:17 -0300

    Update updateCatalog.sh

--- updateCatalog.sh
+++ updateCatalog.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 if [[ -n $(git diff-tree --no-commit-id --name-only -r $GIT_COMMIT|grep catalog.json) ]]; then
   echo Updating App Catalog now after changes made to catalog.json
 	curl -X PUT http://appcatalog.azurewebsites.net/appcatalog/entries -d @catalog.json -H "Content-type: application/json"
