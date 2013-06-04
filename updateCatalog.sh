ebd0fe112dacebf4478cd2eac17c4dfc7a745743
jogoshugh <jgough@JGough.corp.versionone.net>
2013-06-04 12:59:28 -0400

    Updated the catalog update script

--- updateCatalog.sh
+++ updateCatalog.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/bash
-if [[ -n $(git diff-tree --no-commit-id --name-only -r $GIT_COMMIT|grep catalog.json) ]]; then
-  echo Updating App Catalog now after changes made to catalog.json
-	curl -X PUT http://appcatalog.azurewebsites.net/appcatalog/entries -d @catalog.json -H "Content-type: application/json"
+#!/bin/bash
+if [[ -n $(git diff-tree --no-commit-id --name-only -r $GIT_COMMIT |grep product.json) ]]; then
+  echo Updating App Catalog staging now after changes made to product.json
+  echo curl -X PUT http://appcatalogstage.azurewebsites.net/entry --user catUser:CatsAreUs -d @product.json -H "Content-type: application/json"
 else
-	echo Skipping App Catalog update since no changes made to catalog.json
+  echo Skipping App Catalog update since no changes made to product.json
 fi
+
