933c9e6b8d9ef5653767699491050be14e8064d7
Ian Buchanan <ian.buchanan@versionone.com>
2013-07-08 21:14:13 -0400

    remove script and update catalog entry

--- updateCatalog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ -n $(git diff-tree --no-commit-id --name-only -r $GIT_COMMIT |grep product.json) ]]; then
-  echo Updating App Catalog staging now after changes made to product.json
-  echo curl -X PUT http://appcatalogstage.azurewebsites.net/entry --user catUser:CatsAreUs -d @product.json -H "Content-type: application/json"
-else
-  echo Skipping App Catalog update since no changes made to product.json
-fi
-
